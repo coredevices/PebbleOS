b25e9d9adf236b3ba4120a4c639b6df525211d17
Joseph Mearman <joseph@mearman.co.uk>
2026-01-08 10:48:41 +0000

    test: add tinymt include path to unit tests
    Signed-off-by: Joseph Mearman <joseph@mearman.co.uk>

--- waftools/pebble_test.py
+++ waftools/pebble_test.py
@@ -320,7 +320,8 @@ def add_clar_test(bld, test_name, test_source, sources_ant_glob, product_sources
                      "third_party/freertos",
                      "third_party/freertos/FreeRTOS-Kernel/FreeRTOS/Source/include",
                      "third_party/freertos/FreeRTOS-Kernel/FreeRTOS/Source/portable/GCC/ARM_CM3",
-                     "third_party/nanopb/nanopb" ]
+                     "third_party/nanopb/nanopb",
+                     "third_party/tinymt/TinyMT/tinymt" ]
 
     # Use Snowy's resource headers as a fallback if we don't override it here
     resource_override_dir_name = platform if platform in ('silk', 'robert') else 'snowy'
