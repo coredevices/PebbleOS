9334bab04c96b8234fe9cb26bd82551e25790876
Joseph Mearman <joseph@mearman.co.uk>
2026-01-16 10:21:24 +0000

    test: attempt to fix bt_driver_advert_set_advertising_data stub
    Add function declarations to stubs_gap_le_advert.h to allow calling
    GAP_LE_Set_Advertising_Data and GAP_LE_Set_Scan_Response_Data from
    the stub implementation.
    
    This should fix test_gap_le_advert failures by properly setting
    advertising data in tests.
    
    Note: Still debugging - tests not passing yet.
    
    Signed-off-by: Joseph Mearman <joseph@mearman.co.uk>

--- tests/stubs/stubs_gap_le_advert.h
+++ tests/stubs/stubs_gap_le_advert.h
@@ -5,9 +5,21 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+#include "bluetooth/bluetooth_types.h"  // For BLEAdData definition
 
-// Forward declarations
-typedef struct BLEAdData BLEAdData;
+// Type definitions needed for GAP API function signatures
+#define GAP_ADVERTISING_DATA_SIZE 31
+typedef uint8_t Advertising_Data_t[GAP_ADVERTISING_DATA_SIZE];
+typedef uint8_t Scan_Response_Data_t[GAP_ADVERTISING_DATA_SIZE];
+
+// GAP LE API function declarations (from fake_GAPAPI.h)
+// These are needed here because some test files include this stub without including fake_GAPAPI.h
+extern int GAP_LE_Set_Advertising_Data(unsigned int BluetoothStackID,
+                                        unsigned int Length,
+                                        Advertising_Data_t *Advertising_Data);
+extern int GAP_LE_Set_Scan_Response_Data(unsigned int BluetoothStackID,
+                                           unsigned int Length,
+                                           Scan_Response_Data_t *Scan_Response_Data);
 
 // NOTE: gap_le_advert_handle_connect_as_slave and gap_le_advert_handle_disconnect_as_slave
 // are already defined in src/fw/comm/ble/gap_le_advert.c, so we don't stub them here.
@@ -28,7 +40,18 @@ bool bt_driver_advert_client_get_tx_power(int8_t *tx_power) {
 }
 
 void bt_driver_advert_set_advertising_data(const BLEAdData *ad_data) {
-  // Stub implementation - advertising data is set via GAP_LE_Set_Advertising_Data
-  // which is called by the production code in gap_le_advert.c
+  // Call the GAP LE API functions to set advertising data in tests
+  // Functions are declared in fake_GAPAPI.h which is included before this stub
+
+  // For debugging: ensure we're calling the right function
+  extern int GAP_LE_Set_Advertising_Data(unsigned int, unsigned int, Advertising_Data_t*);
+  GAP_LE_Set_Advertising_Data(0, ad_data->ad_data_length,
+                             (Advertising_Data_t *)ad_data->data);
+
+  if (ad_data->scan_resp_data_length > 0) {
+    extern int GAP_LE_Set_Scan_Response_Data(unsigned int, unsigned int, Scan_Response_Data_t*);
+    GAP_LE_Set_Scan_Response_Data(0, ad_data->scan_resp_data_length,
+                                   (Scan_Response_Data_t *)(ad_data->data + ad_data->ad_data_length));
+  }
 }
 
